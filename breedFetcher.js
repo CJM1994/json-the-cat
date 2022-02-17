78900bbc25346ccec6a86d1a809bcfc53770d097
CJM1994 <connorjohnmullin@gmail.com>
2022-02-17 15:34:57 -0800

    added error if server cannot be reached

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,8 +4,7 @@ const searchBreed = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${searchBreed}`, (error, response, body) => {
 
-  // console.log('error: ', error);
-  // console.log('resonse: ', response);
+  if (error) throw new Error('Request to server failed, check request URL (line 5) and that TheCatAPI is active');
 
   if (JSON.parse(body)[0] === undefined) {
     console.log('Error: Breed not found');
@@ -14,9 +13,5 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${searchBreed}`, (error, r
     const breedDescription = JSON.parse(body)[0].description;
     console.log('Breed Description: ', breedDescription);
   }
-
-  // if (breedDescription)
-  //   console.log('Breed Description: ', breedDescription);
-  // else console.log('Breed Name Invalid');
 
 })
